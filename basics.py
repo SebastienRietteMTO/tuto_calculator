aa99edeae8a03ab1fc2160950fbcc597bdc08b74
Sébastien Riette <sebastien.riette@meteo.fr>
2025-11-24 11:31:31 +0100

    First version

---
+++ basics.py
@@ -0,0 +1,14 @@
+"""
+Calculator module
+"""
+
+class Calculator():
+    """
+    Calculator class implementing simple operations
+    """
+
+    def add(self, a, b):
+        """
+        Returns the addition of a with b
+        """
+        return a+b
